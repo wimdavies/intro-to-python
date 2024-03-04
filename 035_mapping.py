c70f0fa980b4759ff013a1b4bfd4a6f4cad4382b
wimdavies <william.johnston.davies@gmail.com>
2024-03-04 16:28:44 +0000

    Complete 035_mapping

--- 035_mapping.py
+++ 035_mapping.py
@@ -35,7 +35,10 @@ print("Function: add_one_hundred_to_numbers")
 
 # Return a new list of each number with 100 added
 def add_one_hundred_to_numbers(numbers):
-  pass
+  new_numbers = []
+  for number in numbers:
+    new_numbers.append(number + 100)
+  return new_numbers
 
 check_that_these_are_equal(
   add_one_hundred_to_numbers([1, 2, 3, 4]), [101, 102, 103, 104])
