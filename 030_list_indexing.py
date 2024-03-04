db4d60ea0f64948b7d5b619f874ae909aa0ad9b5
wimdavies <william.johnston.davies@gmail.com>
2024-03-04 12:26:18 +0000

    Complete 030_list_indexing

--- 030_list_indexing.py
+++ 030_list_indexing.py
@@ -22,8 +22,7 @@ print("")
 print("Function: get_first_item")
 
 def get_first_item(the_list):
-  # Return the first item of the list
-  pass
+  return the_list[0]
 
 check_that_these_are_equal(
   get_first_item(["a", "b", "c", "d", "e"]),
@@ -41,8 +40,7 @@ print("")
 print("Function: get_last_item")
 
 def get_last_item(the_list):
-  # Return the last item of the list
-  pass
+  return the_list[-1]
 
 check_that_these_are_equal(
   get_last_item(["a", "b", "c", "d", "e"]),
@@ -60,8 +58,7 @@ print("")
 print("Function: get_nth_item")
 
 def get_nth_item(the_list, n):
-  # Return the item of the list at the specified index
-  pass
+  return the_list[n]
 
 check_that_these_are_equal(
   get_nth_item(["a", "b", "c", "d", "e"], 3),
@@ -79,9 +76,7 @@ print("")
 print("Function: get_items_between_one_and_three")
 
 def get_items_between_one_and_three(the_list):
-  # Return the section of the list between indexes one
-  # and three
-  pass
+  return the_list[1:3]
 
 check_that_these_are_equal(
   get_items_between_one_and_three(["a", "b", "c", "d", "e"]),
