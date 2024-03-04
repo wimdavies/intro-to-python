fef4d8847b3d2d7ee2194e4f6f853d64f40ddaa7
wimdavies <william.johnston.davies@gmail.com>
2024-03-04 12:13:03 +0000

    Complete 028_logic

--- 028_logic.py
+++ 028_logic.py
@@ -59,8 +59,7 @@ print("")
 print("Function: a_and_b")
 
 def a_and_b(a, b):
-  # return a ?? b
-  pass
+  return a and b
 
 check_that_these_are_equal(a_and_b(True, True), True)
 check_that_these_are_equal(a_and_b(True, False), False)
@@ -75,8 +74,7 @@ print("Function: not_a")
 # Note that this operator only takes one value. The operator
 # goes first, and the value second.
 def not_a(a):
-  # return ?? a
-  pass
+  return not a
 
 check_that_these_are_equal(not_a(True), False)
 check_that_these_are_equal(not_a(False), True)
